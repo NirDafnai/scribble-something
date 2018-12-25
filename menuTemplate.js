b7d5bd9a1ba06a7f9af8405c336fa95d4ec92df3
Nir Dafnai <nirdafnai3@gmail.com>
2018-12-25 09:29:03 +0200

    Update 1
    Cleaned up code and added basic drawing.

---
+++ menuTemplate.js
@@ -0,0 +1,50 @@
+function createMenu() 
+{
+    // Build menu from template
+    let mainMenuTemplate = [
+        {
+            label:'File',
+            submenu:[
+                {
+                    label: 'Quit',
+                    accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                    click() {
+                        app.quit();
+                    }
+                }
+            ]
+        }
+    ];
+    // Add developer tools item if not in prod
+    if(process.env.NODE_ENV !== 'production') {
+        mainMenuTemplate.push({
+            label: 'Developer Tools',
+            submenu: [
+                {
+                    label: 'Toggle Developer Tools',
+                    accelerator: process.platform == 'darwin' ? 'Command+Shift+I' : 'Ctrl+Shift+I',
+                    click(item, focusedWindow) {
+                        focusedWindow.toggleDevTools();
+                    }
+                },
+                {
+                    role: 'reload'
+                }
+            ]
+        });
+    }
+    // If macOS, add empty object to menu.
+    if (process.platform == "darwin") {
+        mainMenuTemplate.unshift({});
+    }
+    return mainMenuTemplate;
+}
+
+function exportVars() 
+{
+    module.exports.getMenu = function() {
+        return createMenu();
+    };
+}
+
+exportVars();
--- public/js/mainWindow.js
+++ public/js/mainWindow.js
@@ -7,12 +7,20 @@ class Canvas
         this.canvas = document.getElementById('drawingCanvas');
         this.context = this.canvas.getContext("2d");
         this.paint;
+        this.userPos = {x: 0, y: 0};
         this.addListeners();
     }
     addListeners() 
     {
-        this.canvas.addEventListener("mousedown", event => {
-            this.mouseDown(event);
+        const self = this;
+        this.canvas.addEventListener("mousedown", function(event) {
+            self.setPosition(event);
+        });
+        this.canvas.addEventListener("mousemove", function(event) {
+            self.draw(event);
+        });
+        this.canvas.addEventListener("mouseenter", function(event) {
+            self.setPosition(event);
         });
     }
     getMouseCoordinates(event, canvas) {
@@ -20,10 +28,26 @@ class Canvas
         const mouseY = event.pageY - canvas.offsetTop;
         return {mouseX, mouseY};
     }
-    mouseDown(event) {
+    draw(event) {
+        if(event.buttons !== 1) return; // If mouse is clicked then exit out of the function.
+        this.context.beginPath();
+        this.context.lineWidth = 5; // width of line
+        this.context.lineCap = "round"; // rounded end cap
+        this.context.moveTo(this.userPos.x, this.userPos.y);
+        this.setPosition(event);
+        this.context.lineTo(this.userPos.x, this.userPos.y);
+        this.context.stroke();
+        /*
+        // When the mouse clicks in the canvas
         const {mouseX, mouseY} = this.getMouseCoordinates(event, this.canvas);
         ipcRenderer.send('mouseCoordinates', mouseX, mouseY);
         this.paint = true;
+        */
+    }
+    setPosition(event) {
+        const {mouseX, mouseY} = this.getMouseCoordinates(event, this.canvas);
+        this.userPos.x = mouseX;
+        this.userPos.y = mouseY;
     }
 }
 function main() {
